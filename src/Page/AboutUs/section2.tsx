649c188d89ee9a7918f0cd1935f50383cca3b26c
locpd2 <locpd2@vng.com.vn>
2022-04-16 15:07:21 +0700

    fix height of section1 and fix navbar

--- src/Page/AboutUs/section2.tsx
+++ src/Page/AboutUs/section2.tsx
@@ -2,27 +2,27 @@ import ImageStore from "../../Store/ImageStore";
 
 const Section2 = () => {
   return (
-    <div className='section2'>
-      <div className='section2-text'>
+    <div className="section2">
+      <div className="section2-text">
         Our name speaks for our mission: <br />
         <strong>Boost up your Return On Investment (ROI).</strong> <br />
-        No other than Growth Marketing service can do that job. We, in <br />
-        AppROI, defines Growth Marketing as the scope of not only <br />
-        acquiring your new customers but also activating, retaining and <br />
-        growing the share of wallet of existing customers.
+        No other than Growth Marketing service can do that job. We, in AppROI,
+        defines Growth Marketing as the scope of not only acquiring your new
+        customers but also activating, retaining and growing the share of wallet
+        of existing customers.
       </div>
-      <div className='section2-our-genes'>
-        <div className='section2-our-genes-title'>
-          <span className='black'>Our</span> Genes
+      <div className="section2-our-genes">
+        <div className="section2-our-genes-title">
+          <span className="black">Our</span> Genes
         </div>
-        <div className='section2-our-genes-desc'>
+        <div className="section2-our-genes-desc">
           <img
             src={ImageStore.aboutUsBanner}
-            loading='lazy'
+            loading="lazy"
             width={588}
             height={192}
-            alt=''
-            className='section2-our-genes-img'
+            alt=""
+            className="section2-our-genes-img"
           ></img>
           <p>
             Our dinstingtive tripod for Growth Marketing <br />
--- src/Page/AboutUs/section3.tsx
+++ src/Page/AboutUs/section3.tsx
@@ -2,54 +2,51 @@ import ImageStore from "../../Store/ImageStore";
 
 const Section3 = () => {
   return (
-    <div className='section3'>
+    <div className="section3">
       <img
-        className='section3-triangle-image'
+        className="section3-triangle-image"
         src={ImageStore.aboutUsTriange}
-        loading='lazy'
-        id='w-node-_53599fae-66c4-6470-6ca0-42e481fa0c05-7cf94638'
+        loading="lazy"
+        id="w-node-_53599fae-66c4-6470-6ca0-42e481fa0c05-7cf94638"
         // sizes='(max-width: 991px) 100vw, 532px'
         // srcSet='images/tamgiac-p-500.png 500w, images/tamgiac.png 532w'
-        alt=''
+        alt=""
       ></img>
-      <div className='section3-content section3-product-market'>
-        <div className='section3-content-title'>
+      <div className="section3-content section3-product-market">
+        <div className="section3-content-title">
           Product-market Fit Approach
         </div>
-        <div className='section3-content-title-quote'>
+        <div className="section3-content-title-quote">
           (over company-centric or consumer-centric one)
         </div>
-        <div className='section3-content-desc'>
-          There's no need to choose black or white while grey <br />
-          option always exists. Our strategic approach is finding <br />
-          the common area of consumer needs/wants/demands <br />
-          and product/service offerings.
+        <div className="section3-content-desc">
+          There's no need to choose black or white while grey option always
+          exists. Our strategic approach is finding the common area of consumer
+          needs / wants / demands and product / service offerings.
         </div>
       </div>
-      <div className='section3-content section3-evidence-base'>
-        <div className='section3-content-title'>Evidence-based decision</div>
-        <div className='section3-content-title-quote'>
+      <div className="section3-content section3-evidence-base">
+        <div className="section3-content-title">Evidence-based decision</div>
+        <div className="section3-content-title-quote">
           (over assumption-based one)
         </div>
-        <div className='section3-content-desc'>
-          We monitors every next action not by somebody's <br />
-          opinions but by data, especially the data closer to the <br />
-          business results, such as acquisition cost on customer <br />
-          lifetime value as well as the data from qualitative <br />
-          customer interviews and surveys.
+        <div className="section3-content-desc">
+          We monitors every next action not by somebody's opinions but by data,
+          especially the data closer to the business results, such as
+          acquisition cost on customer lifetime value as well as the data from
+          qualitative customer interviews and surveys.
         </div>
       </div>
-      <div className='section3-content section3-agile'>
-        <div className='section3-content-title'>Agile marketing operation</div>
-        <div className='section3-content-title-quote'>
+      <div className="section3-content section3-agile">
+        <div className="section3-content-title">Agile marketing operation</div>
+        <div className="section3-content-title-quote">
           (over annual planning)
         </div>
-        <div className='section3-content-desc'>
-          Given that market conditions are evolving so quickly <br />
-          that marketing needs a more reactive planning cycle, <br />
-          our growth marketing service needs to follow suit in <br />
-          that hypotheses are formed quarterly, new tactics and <br />
-          experiments are prioritized and executed on a monthly <br />
+        <div className="section3-content-desc">
+          Given that market conditions are evolving so quickly that marketing
+          needs a more reactive planning cycle, our growth marketing service
+          needs to follow suit in that hypotheses are formed quarterly, new
+          tactics and experiments are prioritized and executed on a monthly
           basis, and tracking & adjustment have to be realtime.
         </div>
       </div>
